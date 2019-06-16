1666d96987add7c2bf418a98258a0dcc3613094a
Ulf Zackrisson <ulmezz@gmail.com>
2019-06-16 16:42:05 +0200

    Add node-sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   constructor(props) {
