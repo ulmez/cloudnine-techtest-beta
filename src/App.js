d9d839de1b5828d2ff1dc52c595ca53222b5fee5
Ulf Zackrisson <ulmezz@gmail.com>
2019-06-20 18:17:55 +0200

    Clean up app component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { Route, Switch, withRouter, Link } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import logo from './logo.svg';
-import './App.scss';
 import SecondPage from './components/secondPage/SecondPage';
 import FirstPage from './components/firstPage/FirstPage';
 import SalongListPage from './components/main/salongListPage/SalongListPage';
 import SalongInfoPage from './components/main/salongInfoPage/SalongInfoPage';
+import './App.scss';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      testValue: ''
-    };
-  }
-
-  componentDidMount() {
-    axios.get(`${process.env.REACT_APP_API_URL}/tests`)
-    .then((result) => {
-      console.log(result.data[0].kul);
-      this.setState({
-        testValue: result.data[0].kul
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  }
-
   render() {
     return (
       <div>
-        {/*<div>
-          <Link to="/first">First</Link><br />
-          <Link to="/second">Second</Link><br />
-          <Link to="/salonglist">Salong list</Link><br />
-          <Link to="/salonginfo">Salong info</Link>
-        </div>*/}
         <Route render={({location}) => (
           <TransitionGroup>
             <CSSTransition
@@ -56,15 +27,9 @@ class App extends Component {
             </CSSTransition>
           </TransitionGroup>
         )} />
-        {/*<Switch>
-            <Route exact path="/salonglist" component={SalongListPage} />
-            <Route exact path="/salonginfo" component={SalongInfoPage} />
-        </Switch>*/}
       </div>
     );
   }
 }
-
-// export default App;
 
 export default withRouter(App);
