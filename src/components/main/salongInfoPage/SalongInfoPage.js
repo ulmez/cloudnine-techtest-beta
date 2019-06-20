9d7e3dd35f6cadd8f60c2b44692d28e6e442c70f
Ulf Zackrisson <ulmezz@gmail.com>
2019-06-20 22:16:55 +0200

    Edited partial to use classes instead of styles in info page

--- src/components/main/salongInfoPage/SalongInfoPage.js
+++ src/components/main/salongInfoPage/SalongInfoPage.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import moment from 'moment';
 import { getSerieOfIconsOnAverageGrade } from '../../../helpers/collection';
-import defaultSalongBackground from '../../../images/default-salong-background.svg';
 import mapMarkerAlt from '../../../images/map-marker-alt.svg';
 import angleLeft from '../../../images/angle-left.svg';
 import clockIcon from '../../../images/clock-icon.svg';
@@ -17,7 +16,6 @@ class SalongInfoPage extends Component {
 
         this.state = {
             filtersVisible: false,
-            // salong: {},
             weekDay: {
                 monday: 'Måndag',
                 tuesday: 'Tisdag',
@@ -36,15 +34,12 @@ class SalongInfoPage extends Component {
     }
 
     componentDidMount() {
-        // console.log(getSerieOfIconsOnAverageGrade(this.props.location.state.grades));
-
         this.setState({
             gradeIcons: getSerieOfIconsOnAverageGrade(this.props.location.state.grades)
         });
     }
 
     toggleFilters() {
-        // console.log('Hello');
         if(this.state.filterAngleDown) {
             this.setState({
                 filterAngleDown: false
@@ -71,7 +66,6 @@ class SalongInfoPage extends Component {
     }
 
     backToList() {
-        // console.log('back');
         this.props.history.push("/salonglist");
     }
 
@@ -79,7 +73,7 @@ class SalongInfoPage extends Component {
         console.log(this.props.location.state);
         return (
             <div className="page">
-                <div style={{padding: '10px', border: '1px solid black', backgroundImage: 'url(' + defaultSalongBackground + ')', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', boxShadow: 'inset 0px -94px 43px -49px rgba(0,0,0,0.9)'}}>
+                <div className="salong-info-page background-design">
                     <div style={{height: '125px', border: '0px solid black', display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start'}}>
                         <div style={{border: '0px solid black', color: 'white'}}><span onClick={this.backToList}><img src={angleLeft} alt="Angle-left" /></span></div>
                         <div style={{border: '0px solid black', color: 'white'}}><img src={heartIcon} alt="Heart-icon" /></div>
@@ -88,23 +82,13 @@ class SalongInfoPage extends Component {
                         <div>
                             <div className="salong-info-page type-header" style={{border: '0px solid black', color: 'white'}}>{this.props.location.state.name}</div>
                             <div style={{border: '0px solid white', color: '#B69F58', fontSize: '14px', paddingTop: '5px'}}>
-                                
                                 {this.state.gradeIcons.map((icon, index) => (
                                     <span key={index} className={icon} style={{width: '17px'}}></span>
                                 ))}
-                                
-                                
-                                {/* <span className="fa fa-star" style={{width: '17px'}}></span>
-                                <span className="fa fa-star" style={{width: '17px'}}></span>
-                                <span className="fa fa-star-half-o" style={{width: '17px'}}></span>
-                                <span className="fa fa-star-o" style={{width: '25px'}}></span> */}
                                 <span style={{color: 'white', fontSize: '13px', paddingLeft: '8px'}}>({this.props.location.state.grades.length})</span>
                             </div>
                         </div>
                     </div>
-                    {/*<Link to="/salonglist">Salong list</Link><br />
-                    Salong info page*/}
-                    {/* <img src={test} alt="test" /> */}
                 </div>
                 <div style={{display: 'flex', height: '50px', border: '0px solid black', fontWeight: '400'}}>
                     <div style={{borderBottom: '2px solid #B69F58', flex: '1', textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><div style={{border: '0px solid black'}}>Info</div></div>
@@ -124,7 +108,6 @@ class SalongInfoPage extends Component {
                             ))}
                         </div>
                     </div>
-                    {/*<div className={this.state.filtersVisible ? 'salong-list-page overflow-hidden show' : 'salong-list-page overflow-hidden hide'}>*/}
                     <hr style={{backgroundColor: '#D8D8D8', height: '1px', border: '0'}} />
                     <div style={{height: '20px', border: '0px solid black', display: 'table'}}><span style={{display: 'table-cell', verticalAlign: 'bottom', width: '25px'}}><img src={phoneIcon} alt="Map-marker-alt" /></span><span style={{display: 'table-cell', verticalAlign: 'middle'}}>{this.props.location.state.phone}</span></div>
                     <hr style={{backgroundColor: '#D8D8D8', height: '1px', border: '0'}} />
@@ -136,7 +119,5 @@ class SalongInfoPage extends Component {
         );
     }
 }
-
-// export default SalongInfoPage;
 
 export default withRouter(SalongInfoPage);
